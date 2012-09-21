1983f1d4ef0ae0c504e11a7fad486a1cbdc59e26
gelosie <gelosie.wang@gmail.com>
2012-09-21 20:40:02 +0800

    生成的html文件，替换.md，而不是在后面添加

--- build.go
+++ build.go
@@ -30,7 +30,7 @@ func (self *Visitor) visit(path string, f os.FileInfo, err error) error {
 			input, _ := ioutil.ReadAll(file)
 			output := blackfriday.MarkdownCommon(input)
 			var out *os.File
-			if out, err = os.Create(f.Name() + ".html"); err != nil {
+			if out, err = os.Create(strings.Replace(f.Name(),".md",".html",-1) ); err != nil {
 				fmt.Fprintf(os.Stderr, "Error creating %s: %v", f.Name(), err)
 				os.Exit(-1)
 			}
